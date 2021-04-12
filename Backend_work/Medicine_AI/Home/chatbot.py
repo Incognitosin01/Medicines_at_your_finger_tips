26ff0d8315bb0fdd40857757a0f313da6237994a
cypherxx <65460055+cypherxx@users.noreply.github.com>
2021-04-12 23:41:31 +0530

    added chatbot

--- Backend_work/Medicine_AI/Home/chatbot.py
+++ Backend_work/Medicine_AI/Home/chatbot.py
@@ -1,40 +1,61 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
-from chatterbot.trainers import ChatterBotCorpusTrainer
+import random
+import json
+import torch
 
-# Creating ChatBot Instance
-chatbot = ChatBot(
-    'MITIAN\'s DOCTOR',
-    storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    logic_adapters=[
-        'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.TimeLogicAdapter',
-        'chatterbot.logic.BestMatch',
-        {
-            'import_path': 'chatterbot.logic.BestMatch',
-            'default_response': 'I am sorry, but I do not understand. I am still learning.',
-            'maximum_similarity_threshold': 0.90
-        }
-    ],
-    database_uri='sqlite:///database.sqlite3'
-) 
 
- # Training with Personal Ques & Ans 
-conversation = [
-    "Hello",
-    "Hi there!",
-    "How are you doing?",
-    "I'm doing great.",
-    "That is good to hear",
-    "Thank you.",
-    "You're welcome."
-]
-# Training with English Corpus Data 
-# trainer_corpus = ChatterBotCorpusTrainer(chatbot)
-# trainer_corpus.train(
-#     'chatterbot.corpus.english'
-# ) 
 
-trainer = ListTrainer(chatbot)
-trainer.train(conversation)
+from .model1 import NeuralNet
+from .nltk_utils import bag_of_words, tokenize
+
+def getresponse(sentence):
+
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    with open('HOME/intents.json', 'r') as json_data:
+        intents = json.load(json_data)
+
+    FILE = "HOME/data.pth"
+    data = torch.load(FILE)
+
+    input_size = data["input_size"]
+    hidden_size = data["hidden_size"]
+    output_size = data["output_size"]
+    all_words = data['all_words']
+    tags = data['tags']
+    model_state = data["model_state"]
+
+    model = NeuralNet(input_size, hidden_size, output_size).to(device)
+    model.load_state_dict(model_state)
+    model.eval()
+
+    
+
+    
+    
+    
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                return random.choice(intent['responses'])
+    else:
+        return "I do not understand"
+
+
+
+
+
 
