30d9f922e8823a6e3e8bf1c839070329c53a88ad
cypherxx <65460055+cypherxx@users.noreply.github.com>
2021-04-13 00:39:25 +0530

    modified phonetic search

--- Backend_work/Medicine_AI/Home/train.py
+++ Backend_work/Medicine_AI/Home/train.py
@@ -5,8 +5,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
-from .nltk_utils import bag_of_words, tokenize, stem
-from .model1 import NeuralNet
+from nltk_utils import bag_of_words, tokenize, stem
+from model1 import NeuralNet
 
 with open('HOME/intents.json', 'r') as f:
     intents = json.load(f)
